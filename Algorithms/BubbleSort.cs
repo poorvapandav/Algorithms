0c89a3e3d899281ef6b0c6d281990de0eb150dd4
Poorva Pandav <poorva.pandav@gmail.com>
2016-07-17 18:44:45 -0400

    Add project files.

---
+++ Algorithms/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BubbleSort
+    { 
+
+        public static int[] Sort(int[] input)
+        {
+            bool isSwitched = false;
+            do
+            {
+                isSwitched = false;
+                // one iteration for switching through array
+                for (int i=0;i<=input.Length-2;i++)
+                {
+                    if(input[i]>input[i+1])
+                    {
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
+                        isSwitched = true;
+                    }
+                }
+
+            } while (isSwitched); //iterate till it is not switched in for loop
+            return input;
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int[] input = { 4, 5, 7, 6 };
+          input=BubbleSort.Sort(input);
+
+            PrintArray(input);
+            Console.ReadKey();
+        }
+
+        public static void PrintArray(int[] input)
+        {
+            foreach (int i in input)
+            {
+                Console.Write("{0}", i);
+            }
+        }
+    }
+}
